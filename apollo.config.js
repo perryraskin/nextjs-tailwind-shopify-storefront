6021345a2fb0898dbb77904f766892bb7cb03574
Perry Raskin <perryraskin@gmail.com>
2020-07-27 22:23:28 -0400

    Prettier auto formatting

--- apollo.config.js
+++ apollo.config.js
@@ -1,17 +1,17 @@
-const config = require("./graphql.config.json"); // eslint-disable-line
+const config = require("./graphql.config.json") // eslint-disable-line
 
-const shopify = config.generates["models/shopify.model.ts"].schema[0];
-const url = Object.keys(shopify)[0];
-const { headers } = shopify[url];
+const shopify = config.generates["models/shopify.model.ts"].schema[0]
+const url = Object.keys(shopify)[0]
+const { headers } = shopify[url]
 
 module.exports = {
   client: {
     service: {
       name: "shopify",
       url,
-      headers,
+      headers
     },
     excludes: ["**/node_modules/**/*"],
-    includes: ["**/*.{ts,tsx}"],
-  },
-};
+    includes: ["**/*.{ts,tsx}"]
+  }
+}
